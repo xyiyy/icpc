ede44deb40e08c5afa4886a566f7df9dcc4c9e08
xyiyy <fraudxyiyy@gmail.com>
2015-08-05 21:06:40 +0800

    multi-university training contest 5

---
+++ ACM/tasks/ACM/hdu/Problem set/hdu5344.cpp
@@ -0,0 +1,20 @@
+#include "../../../../library/lib.hpp"
+
+class hdu5344 {
+public:
+	void solve(std::istream& in, std::ostream& out) {
+		int t;
+		in>>t;
+		while(t--){
+			ll m,z,l,n;
+			in >> n >> m >> z >> l;
+			ll x = 0;
+			ll ans = 0;
+			rep(i,n){
+				ans ^= (x + x);
+				x = (x * m + z) % l;
+			}
+			out << ans << endl;
+		}
+	}
+};
