50cc14143a5d9914850a6bd2300316927aaef2e6
xyiyy <fraudxyiyy@gmail.com>
2015-11-20 20:24:22 +0800

    dc3 and extend kmp

---
+++ ACM/tasks/ACM/xyiyy/math/BellNumber.cpp
@@ -0,0 +1,15 @@
+int B[4010];
+void Bell(int T[],int N,int MOD)
+{
+    B[0] = 1;
+    B[1] = 1;
+    T[0] = 1;
+    for(int i=2;i<N;i++)
+    {
+        T[i-1] = B[i-1];
+        for(int j=i-2;j>=0;j--)
+            T[j] = (T[j]+T[j+1])%MOD;
+        B[i] = T[0];
+    }
+}
+
