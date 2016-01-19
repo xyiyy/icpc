97f9a8e987fefd4e71e9c91f5ea03b634484ebe0
xyiyy <fraudxyiyy@gmail.com>
2016-01-19 23:50:06 +0800

    facebook hackercup

--- src/Facebook Hacker Cup/2016/Round1/C.cpp
+++ src/Facebook Hacker Cup/2016/Round1/C.cpp
@@ -20,37 +20,84 @@ typedef vector<PII> VII;
 typedef vector<int> VI;
 ll c[100010];
 double dp[100010];
+
+/*
+1
+5 40 140
+4 9 1 12 7
+
+*/
 int main()
 {
     ios::sync_with_stdio(false);
     #ifdef LOCAL
-        //freopen("in3.txt","r",stdin);
-        //freopen("out3.txt","w",stdout);
+        freopen("in3.txt","r",stdin);
+        freopen("out3.txt","w",stdout);
 
     #endif // LOCAL
 
-    int t;
+    int t,cas = 1;
     scanf("%d",&t);
     while(t--){
         int n;
         ll a,b;
         scanf("%d%lld%lld",&n,&a,&b);
         ll tot = 0;
-        double sum = 0;
-        double len = b - a;
-        double ans = 0;
-        double r = 0;
+        long double sum = 0;
+        long double len = b - a;
+        long double ans = 0;
+        long double R = 0;
+        unsigned long long temp = 0;
         rep(i,n){
             scanf("%lld",&c[i]);
             tot += c[i];
-            r = c[i] / 2.0 * (c[i] / len);
+            dp[i] = c[i] / 2.0 * c[i] / len;
+            temp +=  c[i] * c[i] ;
         }
+        //cout << temp << " " << len << endl;
+        R = temp / (2 * len);
+        //R = tot / 2.0;
+        //cout << R <<endl;
+        //cout << tot << " " << R << endl;
         int lnum = 0,rnum = 0;
         lnum = a / tot;
         rnum = b / tot;
-        if(lnum == rnum){
-
+        int l = 0;
+        int r = 0;
+        ll x = a % tot;
+        ll y = b % tot;
+        for(;c[l]<=x;l++){
+            x -= c[l];
         }
+        for(;c[r]<=y;r++){
+            y-=c[r];
+        }
+        ans = ans + (c[l] + x) / 2.0 * ((c[l] - x) / len);
+        l++;
+        //cout << ans << endl;
+        if(lnum < rnum){
+            while(l < n){
+                ans += dp[l];
+                l++;
+            }
+        }else{
+            while(l < r){
+                ans += dp[l];
+                l++;
+            }
+        }
+        //cout << ans << endl;
+        //cout << lnum << " " << rnum << endl;
+        if(l==n){
+            ans += (rnum - lnum - 1)  * R;
+            //cout << ans << endl;
+            rep(i,r){
+                ans += dp[i];
+            }
+        }
+        ans += (y / 2.0) * (y / len);
+        cout << "Case #" << cas++ << ": " <<  fixed << setprecision(12) << ans << endl;
+
 
 
 
